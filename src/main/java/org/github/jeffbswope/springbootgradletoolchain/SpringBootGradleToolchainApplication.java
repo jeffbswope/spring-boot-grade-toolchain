f384acc0b0aff18c884e8be23c5bb9df1a10009c
Jeff Swope <jeff.swope@bioclinica.com>
2020-12-14 19:50:20 -0600

    Initial commit from initializr

---
+++ src/main/java/org/github/jeffbswope/springbootgradletoolchain/SpringBootGradleToolchainApplication.java
@@ -0,0 +1,13 @@
+package org.github.jeffbswope.springbootgradletoolchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleToolchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradleToolchainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/github/jeffbswope/springbootgradletoolchain/SpringBootGradleToolchainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.jeffbswope.springbootgradletoolchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGradleToolchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
